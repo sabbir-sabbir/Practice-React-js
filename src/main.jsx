811d10de857b6af5e7b8725c4246a99577aac24b
sabbir-sabbir <iosabbir7890@gmail.com>
2025-01-18 19:59:38 +0600

    finally i move on with my new projects...

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-import {Provider} from "react-redux"
-import store from '/src/Redux/Store.js';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <strict>
     <App />
-  </Provider>,
+  </strict>,
 )
