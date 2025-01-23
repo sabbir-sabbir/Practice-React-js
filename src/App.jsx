5a132370724f13f7ffac8f0d98210fd7a96e04ed
sabbir-sabbir <iosabbir7890@gmail.com>
2025-01-23 16:02:10 +0600

    ccccc

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css'
-import Hero from './sections/Hero'
-import Navbar from './sections/Navbar'
 
 
 function App() {
 
   return (
     <>
-     <main className="max-w-7xl mx-auto">
-      <Navbar/>
-      <Hero/>
-     </main>
+  
     </>
   )
 }
